61f936563b6193518637a883c460870e253df35a
Kurtis Moffett <moffkr91@gmail.com>
2018-04-03 19:33:21 -0700

    writing spotify response in new file now

--- liri.js
+++ liri.js
@@ -12,11 +12,20 @@ let  spotify = new Spotify({
   });
 
 if (command === 'spotify-this-song') {
-
     spotify.search({ type: 'track', query: input, limit:1 }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
         }
-        console.log(JSON.stringify(data).split(','))
+
+        let track_string = JSON.stringify(data.tracks.items)
+        let track_obj = JSON.parse(track_string)
+        
+        fs.writeFile('./spotify_string.txt', track_string, 'utf8', (error) => {
+            if (error){
+            console.log(error)
+            throw "NOPE"
+            }
+        })
+       
     })
 }
