d99e8aab2bb82845652a505e2119a4610dae2216
Kurtis Moffett <moffkr91@gmail.com>
2018-04-06 15:09:55 -0700

    now saving log, as well as the command called for that instance

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ let [node, liri, command, ...input] = process.argv
 
 
 if (command === 'spotify-this-song') {
+  if(input[0] === undefined) {input = 'The-Sign+Ace-of-Base'}
     spotify.search({ type: 'track', query: input, limit: 1 }, function(err, data) {
         if (err) {
           return console.log('Error occurred: ' + err);
@@ -18,23 +19,23 @@ if (command === 'spotify-this-song') {
         let album = JSON.stringify(data.tracks.items[0].album.name)
         let final_data = '\r\nArtist: '+artist+'\r\nSong Title: '+track_name+'\r\nSong Preview: '+preview+'\r\nAlbum Title: '+album
 
-        fs.appendFile('./spotify_songz.txt', final_data , (err) => {
+        fs.appendFile('./log.txt', '\r\n'+command+':'+input+final_data , (err) => {
             if (err) throw err;
           });
           console.log(final_data)
     })
-    console.log(input)
 }
 
 if (command === 'movie-this') {
-  request('http://www.omdbapi.com/?apikey='+keys.omdb.key+'&t='+input+'&r=json&plot=short&', function (error, response, body) {
-    if (error) {console.log('error:', error)} 
-    let movie_data = JSON.parse(body)
-    let final_data = '\r\nTitle: '+movie_data.Title+'\r\nRelease Date: '+movie_data.Year+'\r\nIMDB Rating: '+movie_data.imdbRating+'\r\n'+movie_data.Ratings[1].Source+': '+movie_data.Ratings[1].Value+'\r\nCountry Produced: '+movie_data.Production+'\r\nLanguage(s): '+movie_data.Language+'\r\nPlot: '+movie_data.Plot+'\r\nActors: '+movie_data.Actors
-    
-  fs.appendFile('./omdb.txt', final_data, (err) => {
-    if (err) throw err;
-    });
-    console.log(final_data)
-    });
+  if(input[0] === undefined) {input = 'Mr. Nobody'}
+    request('http://www.omdbapi.com/?apikey='+keys.omdb.key+'&t='+input+'&r=json&plot=short&', function (error, response, body) {
+      if (error) {console.log('error:', error)} 
+      let movie_data = JSON.parse(body)
+      let final_data = '\r\nTitle: '+movie_data.Title+'\r\nRelease Date: '+movie_data.Year+'\r\nIMDB Rating: '+movie_data.imdbRating+'\r\n'+movie_data.Ratings[1].Source+': '+movie_data.Ratings[1].Value+'\r\nCountry Produced: '+movie_data.Production+'\r\nLanguage(s): '+movie_data.Language+'\r\nPlot: '+movie_data.Plot+'\r\nActors: '+movie_data.Actors
+      
+    fs.appendFile('./log.txt', '\r\n'+command+':'+input+final_data, (err) => {
+      if (err) throw err;
+      });
+      console.log(final_data)
+      });
 }
