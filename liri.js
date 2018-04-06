e6e0cf1c40c3ff43808833cf5f0093f48d28a5ec
Kurtis Moffett <moffkr91@gmail.com>
2018-04-06 11:52:04 -0700

    appending history

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ let Spotify = require('node-spotify-api');
 let  spotify = new Spotify(keys.spotify);
 let [node, liri, command, ...input] = process.argv
 
+
 if (command === 'spotify-this-song') {
     spotify.search({ type: 'track', query: input, limit: 1 }, function(err, data) {
         if (err) {
@@ -15,31 +16,25 @@ if (command === 'spotify-this-song') {
         let track_name = JSON.stringify(data.tracks.items[0].name)
         let preview = JSON.stringify(data.tracks.items[0].preview_url)
         let album = JSON.stringify(data.tracks.items[0].album.name)
+        let final_data = '\r\nArtist: '+artist+'\r\nSong Title: '+track_name+'\r\nSong Preview: '+preview+'\r\nAlbum Title: '+album
 
-        fs.writeFile('./spotify_songz.txt', 'Artist: '+artist+'\r\nSong Title: '+track_name+'\r\nSong Preview: '+preview+'\r\nAlbum Title: '+album, (err) => {
+        fs.appendFile('./spotify_songz.txt', final_data , (err) => {
             if (err) throw err;
           });
-
-        fs.readFile('./spotify_songz.txt', (err, data) => {
-            if (err) throw err;
-            console.log(data.toString('utf-8'));
-          });  
+          console.log(final_data)
     })
+    console.log(input)
 }
 
 if (command === 'movie-this') {
   request('http://www.omdbapi.com/?apikey='+keys.omdb.key+'&t='+input+'&r=json&plot=short&', function (error, response, body) {
-  if (error) {console.log('error:', error)} 
-  let movie_data = JSON.parse(body)
-  let final_data = 'Title: '+movie_data.Title+'\r\nRelease Date: '+movie_data.Year+'\r\nIMDB Rating: '+movie_data.imdbRating+'\r\n'+movie_data.Ratings[1].Source+': '+movie_data.Ratings[1].Value+'\r\nCountry Produced: '+movie_data.Production+'\r\nLanguage(s): '+movie_data.Language+'\r\nPlot: '+movie_data.Plot+'\r\nActors: '+movie_data.Actors
-  
-  fs.writeFile('./omdb.txt', final_data, (err) => {
+    if (error) {console.log('error:', error)} 
+    let movie_data = JSON.parse(body)
+    let final_data = '\r\nTitle: '+movie_data.Title+'\r\nRelease Date: '+movie_data.Year+'\r\nIMDB Rating: '+movie_data.imdbRating+'\r\n'+movie_data.Ratings[1].Source+': '+movie_data.Ratings[1].Value+'\r\nCountry Produced: '+movie_data.Production+'\r\nLanguage(s): '+movie_data.Language+'\r\nPlot: '+movie_data.Plot+'\r\nActors: '+movie_data.Actors
+    
+  fs.appendFile('./omdb.txt', final_data, (err) => {
     if (err) throw err;
     });
-
-  fs.readFile('./omdb.txt', (err, data) => {
-    if (err) throw err;
-    console.log(data.toString('utf-8'));
-    }); 
-  });
+    console.log(final_data)
+    });
 }
